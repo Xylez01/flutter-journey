f56a70ea5dbab7efe7923b7d48460411685ad618
Jordy Langen <jordy.langen@signify.com>
2022-02-11 14:01:19 +0100

    Add example app

---
+++ example/lib/main.dart
@@ -0,0 +1,161 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:journey/journey.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'migrations.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  runApp(JourneyExampleApp(
+    sharedPreferences: sharedPreferences,
+  ));
+}
+
+class JourneyExampleApp extends StatelessWidget {
+  const JourneyExampleApp({
+    required this.sharedPreferences,
+    Key? key,
+  }) : super(key: key);
+
+  final SharedPreferences sharedPreferences;
+
+  @override
+  Widget build(BuildContext context) {
+    final style = sharedPreferences.getString("style") ?? "purple";
+
+    return MaterialApp(
+      title: 'Journey example app',
+      theme: ThemeData(
+        primarySwatch:
+            style == "purple" ? Colors.deepPurple : Colors.deepOrange,
+      ),
+      home: Scaffold(
+        body: JourneyHomepage(
+          sharedPreferences: sharedPreferences,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class JourneyHomepage extends StatefulWidget {
+  const JourneyHomepage({
+    required this.sharedPreferences,
+    Key? key,
+  }) : super(key: key);
+
+  final SharedPreferences sharedPreferences;
+
+  @override
+  State<JourneyHomepage> createState() => _JourneyHomepageState();
+}
+
+class _JourneyHomepageState extends State<JourneyHomepage> {
+  late Journey _journey;
+
+  @override
+  Widget build(BuildContext context) {
+    final backgroundImage =
+        widget.sharedPreferences.getString("background_image") ??
+            "background-purple.jpg";
+
+    final style = widget.sharedPreferences.getString("style") ?? "purple";
+
+    final migrations = {
+      "Migrate to new style": MigrateToNewBackgroundImage(
+        sharedPreferences: widget.sharedPreferences,
+      ),
+      "Migrate to new name": MigrateToNewStyle(
+        sharedPreferences: widget.sharedPreferences,
+      ),
+    };
+
+    _journey = Journey(
+      migrations: migrations.values.toList(),
+    );
+
+    return Stack(
+      children: [
+        SizedBox.expand(
+          child: Image(
+            image: AssetImage("images/$backgroundImage"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              widget.sharedPreferences.getString("app_name") ?? "Journey",
+              style: const TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(2.0, 2.0),
+                    blurRadius: 3.0,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: ElevatedButton(
+                      child: const Text("Migrate"),
+                      onPressed: () async {
+                        await _journey.migrate();
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: ElevatedButton(
+                      child: const Text("Reset"),
+                      onPressed: () async {
+                        await widget.sharedPreferences.clear();
+
+                        final documentDirectory =
+                            await getApplicationDocumentsDirectory();
+
+                        final reportsFile = File(
+                            "${documentDirectory.path}/journey/reports.json");
+
+                        if (reportsFile.existsSync()) {
+                          reportsFile.deleteSync();
+                        }
+
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
